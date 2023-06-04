b5d4e8b1ab8f8177f3047a21478218af9ddecb97
Ayangbile Daniel Bayo <65937766+DanielBayo@users.noreply.github.com>
2023-06-05 00:41:33 +0100

    Create QuizQuestion.py

---
+++ QuizQuestion.py
@@ -0,0 +1,5 @@
+#The prompt attributes refers to the Questions while answer refers to the answer to the Question
+class Question:
+  def __init__(self,prompt,answer):
+    self.prompt = prompt
+    self.answer = answer
