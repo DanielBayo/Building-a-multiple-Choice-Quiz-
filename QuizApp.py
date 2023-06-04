0f071dc9e4f44c7816a15f37e81d12b3f59ae139
Ayangbile Daniel Bayo <65937766+DanielBayo@users.noreply.github.com>
2023-06-05 00:59:09 +0100

    Update QuizApp.py

--- QuizApp.py
+++ QuizApp.py
@@ -1,1 +1,5 @@
-
+question_prompt= [
+  "Which chess piece is of the lowest relative value?\n(a) Pawn\n(b) Bishop\n(c) Knight\n(d) Castle\n\n",
+  "Which musical instrument was invented by Belgian Adolphe Sax?\n(a) Tuba\n(b) Trumpet\n(c) Saxophone\n(d) Euphonium\n\n",
+  "Which what is the Capital of Canada?\n(a) Monthreal\n(b) Ottawa\n(c) Toronto\n(d) Vancouver\n\n"
+]
